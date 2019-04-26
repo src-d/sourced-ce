9747eb82cee36186303c85f8a04af693857bec42
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-04-26 19:15:29 +0200

    Add install, start, stop, prune commands
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

---
+++ cmd/sandbox-ce/cmd/install.go
@@ -0,0 +1,32 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/smacker/superset-compose/cmd/sandbox-ce/compose"
+)
+
+type installCmd struct {
+	Command `name:"install" short-description:"Install"`
+}
+
+func (c *installCmd) Execute(args []string) error {
+	err := compose.Run(context.Background(),
+		"run", "--rm", "superset", "./docker-init.sh")
+
+	if err != nil {
+		return err
+	}
+
+	err = compose.Run(context.Background(), "up")
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func init() {
+	rootCmd.AddCommand(&installCmd{})
+}
