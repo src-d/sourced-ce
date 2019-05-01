1fe4041c55bf7602495298845b56f4c6397c0443
Maxim Sukharev <max@smacker.ru>
2019-05-01 19:48:53 +0200

    add gitbase tables
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

--- docker/add_gitbase.py
+++ docker/add_gitbase.py
@@ -1,5 +1,6 @@
 from superset import conf, db
 from superset.models import core as models
+from superset.connectors.connector_registry import ConnectorRegistry
 
 
 def get_or_create_gitbase_db():
@@ -17,5 +18,32 @@ def get_or_create_gitbase_db():
     db.session.add(dbobj)
     db.session.commit()
 
+    return dbobj
+
+
+def add_gitbase_tables():
+    dbobj = get_or_create_gitbase_db()
+    TBL = ConnectorRegistry.sources['table']
+    for table in dbobj.all_table_names_in_schema('gitbase'):
+        # table_name should match the one in the datasource for fetch_metadata to work
+        if db.session.query(TBL).filter_by(table_name=table).first():
+            continue
+        if db.session.query(TBL).filter_by(table_name='gitbase.' + table).first():
+            continue
+
+        # create table with original name and fetch columns
+        tbl = TBL(table_name=table)
+        tbl.database = dbobj
+        db.session.add(tbl)
+        db.session.commit()
+        tbl.fetch_metadata()
+
+        # rename with prefix and set source
+        tbl.table_name = 'gitbase.' + table
+        tbl.sql = 'select * from ' + table
+        db.session.add(dbobj)
+        db.session.commit()
+
 
 get_or_create_gitbase_db()
+add_gitbase_tables()
