20a1993f459c753a7d38d1f4954ed1e272acb281
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-05-07 12:25:48 +0200

    Add opening of UI after install command
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- cmd/sandbox-ce/cmd/install.go
+++ cmd/sandbox-ce/cmd/install.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"context"
+	"time"
 
 	"github.com/src-d/superset-compose/cmd/sandbox-ce/compose"
 )
@@ -11,15 +12,18 @@ type installCmd struct {
 }
 
 func (c *installCmd) Execute(args []string) error {
-	err := compose.Run(context.Background(),
-		"run", "--rm", "superset", "./docker-init.sh")
-
-	if err != nil {
+	if err := compose.Run(context.Background(),
+		"run", "--rm", "superset", "./docker-init.sh"); err != nil {
 		return err
 	}
 
-	err = compose.Run(context.Background(), "up", "--detach")
-	if err != nil {
+	done := OpenUI(10 * time.Second)
+
+	if err := compose.Run(context.Background(), "up", "--detach"); err != nil {
+		return err
+	}
+
+	if err := <-done; err != nil {
 		return err
 	}
 
