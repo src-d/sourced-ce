d36f5f1d17e7efe0fb8ffee2fa060c19e4a593ef
Maxim Sukharev <max@smacker.ru>
2019-05-16 17:47:14 +0200

    Add status command
    fix: #14
    
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

---
+++ cmd/sandbox-ce/cmd/status.go
@@ -0,0 +1,19 @@
+package cmd
+
+import (
+	"context"
+
+	"github.com/src-d/superset-compose/cmd/sandbox-ce/compose"
+)
+
+type statusCmd struct {
+	Command `name:"status" short-description:"Shows status of the components"`
+}
+
+func (c *statusCmd) Execute(args []string) error {
+	return compose.Run(context.Background(), "ps")
+}
+
+func init() {
+	rootCmd.AddCommand(&statusCmd{})
+}
