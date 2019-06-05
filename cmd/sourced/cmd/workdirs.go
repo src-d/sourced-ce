49cd7b46d1571650712b91e275530e6e4e6fe4e2
Maxim Sukharev <max@smacker.ru>
2019-06-05 17:03:31 +0200

    rename list command to workdirs
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

--- cmd/sourced/cmd/workdirs.go
+++ cmd/sourced/cmd/workdirs.go
@@ -6,11 +6,11 @@ import (
 	"github.com/src-d/sourced-ce/cmd/sourced/compose/workdir"
 )
 
-type listCmd struct {
-	Command `name:"list" short-description:"List working directories" long-description:"List previously initialized working directories."`
+type workdirsCmd struct {
+	Command `name:"workdirs" short-description:"List working directories" long-description:"List previously initialized working directories."`
 }
 
-func (c *listCmd) Execute(args []string) error {
+func (c *workdirsCmd) Execute(args []string) error {
 	dirs, err := workdir.ListRepoDirs()
 	if err != nil {
 		return err
@@ -33,5 +33,5 @@ func (c *listCmd) Execute(args []string) error {
 }
 
 func init() {
-	rootCmd.AddCommand(&listCmd{})
+	rootCmd.AddCommand(&workdirsCmd{})
 }
