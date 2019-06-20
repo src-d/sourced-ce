8db677d5121e6c0579e972eab64d77aa761ef9a5
Maxim Sukharev <max@smacker.ru>
2019-06-20 15:20:26 +0200

    Implement token and orgs validation
    Fix: #59
    
    Signed-off-by: Maxim Sukharev <max@smacker.ru>

--- cmd/sourced/cmd/orgs.go
+++ cmd/sourced/cmd/orgs.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"context"
 	"fmt"
+	"os"
 	"strings"
 	"time"
 
@@ -40,6 +41,19 @@ func (c *orgsInitCmd) Execute(args []string) error {
 	}
 
 	fmt.Printf("docker-compose working directory set to %s\n", strings.Join(c.Args.Orgs, ","))
+
+	err = compose.Run(context.Background(), "run",
+		// have to override endpoint:
+		// $ docker-compose run --rm --no-deps ghsync validate
+		// /bin/sh: can't open 'validate': No such file or directory
+		// $ docker-compose run --rm --no-deps --entrypoint /bin/ghsync ghsync validate
+		// github token is not valid
+		"--entrypoint", "/bin/ghsync",
+		"--rm", "--no-deps", "ghsync", "validate")
+	if err != nil {
+		// avoid duplicated "exit status 1" message
+		os.Exit(1)
+	}
 
 	if err := compose.Run(context.Background(), "up", "--detach"); err != nil {
 		return err
