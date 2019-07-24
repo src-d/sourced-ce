cb664684fbfccdc9e15c2a25c4358899461d225a
David Pordomingo <David.Pordomingo.F@gmail.com>
2019-07-24 15:11:50 +0200

    Make sure that ~/.sourced dir is ready for all sourced commands
    Signed-off-by: David Pordomingo <David.Pordomingo.F@gmail.com>

--- cmd/sourced/dir/dir.go
+++ cmd/sourced/dir/dir.go
@@ -16,10 +16,31 @@ import (
 // ErrNotExist is returned when .sourced dir does not exists
 var ErrNotExist = goerrors.NewKind("%s does not exist")
 
+// ErrNotValid is returned when config dir is not valid
+var ErrNotValid = goerrors.NewKind("%s is not a valid config directory: %s")
+
 // Path returns the absolute path for $SOURCED_DIR, or $HOME/.sourced if unset
 func Path() (string, error) {
+	srcdDir, err := path()
+	if err != nil {
+		return "", err
+	}
+
+	if err := validate(srcdDir); err != nil {
+		return "", err
+	}
+
+	return srcdDir, nil
+}
+
+func path() (string, error) {
 	if d := os.Getenv("SOURCED_DIR"); d != "" {
-		return filepath.Abs(d)
+		abs, err := filepath.Abs(d)
+		if err != nil {
+			return "", errors.Wrap(err, fmt.Sprintf("could not resolve SOURCED_DIR='%s'", d))
+		}
+
+		return abs, nil
 	}
 
 	homedir, err := os.UserHomeDir()
@@ -27,13 +48,46 @@ func Path() (string, error) {
 		return "", errors.Wrap(err, "could not detect home directory")
 	}
 
-	srcdDir := filepath.Join(homedir, ".sourced")
-	_, err = os.Lstat(srcdDir)
-	if os.IsNotExist(err) {
-		return "", ErrNotExist.New(srcdDir)
+	return filepath.Join(homedir, ".sourced"), nil
+}
+
+// Prepare tries to create the config directory, returning an error if it could not
+// be created, or nill if already exist or was successfully created.
+func Prepare() error {
+	srcdDir, err := path()
+	if err != nil {
+		return err
 	}
 
-	return srcdDir, nil
+	err = validate(srcdDir)
+	if ErrNotExist.Is(err) {
+		return os.MkdirAll(srcdDir, os.ModePerm)
+	}
+
+	return err
+}
+
+// validate validates that the passed config dir path is valid
+func validate(path string) error {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return ErrNotExist.New(path)
+	}
+
+	if err != nil {
+		return ErrNotValid.New(path, err)
+	}
+
+	readWriteAccessMode := os.FileMode(0700)
+	if info.Mode()&readWriteAccessMode != readWriteAccessMode {
+		return ErrNotValid.New(path, "it has no read-write access")
+	}
+
+	if !info.IsDir() {
+		return ErrNotValid.New(path, "it is not a directory")
+	}
+
+	return nil
 }
 
 // DownloadURL downloads the given url to a file to the
