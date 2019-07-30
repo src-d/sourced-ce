e3b9e9e348fdf5aae52e629b8ed1a11c73053874
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-07-30 20:36:05 +0200

    Remove unnecessary creation of dirs
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- run-integration-tests.bat
+++ run-integration-tests.bat
@@ -1,8 +1,6 @@
 :: we can't use makefile for windows because it depends on CI makefile which depends on shell
 
 :: compile sourced-ce
-if not exist build mkdir build
-if not exist build/sourced-ce_windows_amd64 mkdir build/sourced-ce_windows_amd64
 go build -tags=forceposix -o build/sourced-ce_windows_amd64/sourced.exe ./cmd/sourced
 
 :: run tests
