bd14b432deadd87e47941c5d4e56ebf8fd78233e
Lou Marvin Caraig <loumarvincaraig@gmail.com>
2019-08-09 00:20:32 +0200

    Set TMPDIR_INTEGRATION_TEST to shorter location
    Signed-off-by: Lou Marvin Caraig <loumarvincaraig@gmail.com>

--- run-integration-tests.bat
+++ run-integration-tests.bat
@@ -4,4 +4,10 @@
 go build -tags=forceposix -o build/sourced-ce_windows_amd64/sourced.exe ./cmd/sourced
 
 :: run tests
+set TMPDIR_INTEGRATION_TEST=C:\tmp
+:: see https://stackoverflow.com/questions/22948189/batch-getting-the-directory-is-not-empty-on-rmdir-command
+del /f /s /q %TMPDIR_INTEGRATION_TEST% 1>nul
+rmdir /s /q %TMPDIR_INTEGRATION_TEST%
+mkdir %TMPDIR_INTEGRATION_TEST%
+set TMP=%TMPDIR_INTEGRATION_TEST%
 go test -timeout 20m -parallel 1 -count 1 -tags="forceposix integration" github.com/src-d/sourced-ce/test/ -v
